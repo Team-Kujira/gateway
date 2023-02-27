cca4517922308668a6edc47a96e3d5023affbc46
Danilo Araújo Silva <danilo.silva@funttastic.com>
2023-02-27 23:24:02 +0000

    Starting to a playground integration test (wip).

---
+++ integration-test/clob/connector/kujira/playground.test.ts
@@ -0,0 +1,33 @@
+import { tx, registry } from 'kujira.js';
+import { GasPrice, SigningStargateClient } from '@cosmjs/stargate';
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+
+it('Kujira Playground', async () => {
+  const RPC_ENDPOINT = 'https://rpc-harpoon.kujira.app';
+  const MNEMONIC = '...';
+
+  const signer = await DirectSecp256k1HdWallet.fromMnemonic(MNEMONIC);
+
+  const client = await SigningStargateClient.connectWithSigner(
+    RPC_ENDPOINT,
+    signer,
+    {
+      registry,
+      gasPrice: GasPrice.fromString('0.00125ukuji'),
+    }
+  );
+
+  const FIN_KUJI_DEMO =
+    'kujira1suhgf5svhu4usrurvxzlgn54ksxmn8gljarjtxqnapv8kjnp4nrsqq4jjh';
+
+  const [account] = await signer.getAccounts();
+
+  const msg = tx.wasm.msgExecuteContract({
+    sender: account.address,
+    contract: FIN_KUJI_DEMO,
+    msg: Buffer.from(JSON.stringify({ submit_order: { price: '210.5' } })),
+    funds: coins(1000000, 'ukuji'),
+  });
+
+  await client.signAndBroadcast(account.address, [msg], 'auto');
+});
