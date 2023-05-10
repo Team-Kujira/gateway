57779bdc9bbaaf0daffe38776059d352009d3be5
Darley Araújo Silva <darley.silva@funttastic.com>
2023-05-10 11:46:29 -0300

    Working in the full-flow.test.ts

--- integration-test/clob/connector/kujira/full-flow.test.ts
+++ integration-test/clob/connector/kujira/full-flow.test.ts
@@ -2083,7 +2083,6 @@ describe('Kujira Full Flow', () => {
       logResponse(response);
     });
 
-    // // TODO Fix!!!
     it('Get all open orders and check that there are no open orders', async () => {
       const request = {
         ownerAddress: ownerAddress,
@@ -2095,6 +2094,8 @@ describe('Kujira Full Flow', () => {
       const response = await kujira.getOrders(request);
 
       logResponse(response);
+
+      expect(response.size).toEqual(0);
     });
 
     it('Settle funds for market 1', async () => {
