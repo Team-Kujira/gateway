9c293433493b7389783ce7dc2b12310972cf6b97
Danilo Araújo Silva <danilo.silva@funttastic.com>
2023-07-08 18:16:32 +0300

    Moving CLOB methods.

--- src/connectors/kujira/kujira.connector.ts
+++ src/connectors/kujira/kujira.connector.ts
@@ -2,13 +2,43 @@ import { Kujira } from './kujira';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Cache, CacheContainer } from 'node-ts-cache';
 import { MemoryStorage } from 'node-ts-cache-storage-memory';
+import {
+  CLOBish,
+  MarketInfo,
+  NetworkSelectionRequest,
+  Orderbook,
+} from '../../services/common-interfaces';
+import {
+  ClobDeleteOrderRequest,
+  ClobGetOrderRequest,
+  ClobGetOrderResponse,
+  ClobMarketsRequest,
+  ClobOrderbookRequest,
+  ClobPostOrderRequest,
+  ClobTickerRequest,
+} from '../../clob/clob.requests';
+import {
+  convertHumingbotMarketNameToMarketName,
+  convertMarketNameToHumingbotMarketName,
+} from './kujira.convertors';
+import { getNotNullOrThrowError } from './kujira.helpers';
+import {
+  GetAllMarketsResponse,
+  OrderSide,
+  OrderType,
+  OwnerAddress,
+  Token,
+  TransactionHash,
+} from './kujira.types';
+import { BigNumber } from 'bignumber.js';
+import { BalanceRequest } from '../../network/network.requests';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const caches = {
   instances: new CacheContainer(new MemoryStorage()),
 };
 
-export class KujiraConnector {
+export class KujiraConnector implements CLOBish {
   chain: string;
 
   network: string;
@@ -37,5 +67,159 @@ export class KujiraConnector {
 
   ready(): boolean {
     return this.kujira && this.kujira.isReady;
+  }
+
+  abiDecoder: any;
+
+  public parsedMarkets: MarketInfo = [];
+
+  async balances(req: BalanceRequest): Promise<Record<string, string>> {
+    const balances = await this.kujira.getBalances({
+      ownerAddress: req.address,
+      tokenSymbols: req.tokenSymbols,
+    });
+
+    const output: Record<string, string> = {};
+
+    for (const balance of balances.tokens.values()) {
+      output[(balance.token as Token).symbol] = balance.free.toString();
+    }
+
+    return output;
+  }
+
+  async deleteOrder(req: ClobDeleteOrderRequest): Promise<{ txHash: string }> {
+    const result = await this.kujira.cancelOrder({
+      id: req.orderId,
+      marketName: convertHumingbotMarketNameToMarketName(req.market),
+      ownerAddress: req.address,
+    });
+
+    return {
+      txHash: getNotNullOrThrowError<TransactionHash>(
+        result.hashes?.cancellation
+      ),
+    };
+  }
+
+  estimateGas(_req: NetworkSelectionRequest): {
+    gasPrice: number;
+    gasPriceToken: string;
+    gasLimit: number;
+    gasCost: number;
+  } {
+    const result = this.kujira.getEstimatedFees({});
+
+    return {
+      gasCost: result.cost.toNumber(),
+      gasLimit: result.limit.toNumber(),
+      gasPrice: result.price.toNumber(),
+      gasPriceToken: result.token,
+    };
+  }
+
+  async loadMarkets(): Promise<void> {
+    const allMarkets =
+      (await this.kujira.getAllMarkets()) as GetAllMarketsResponse;
+
+    for (const market of allMarkets.values()) {
+      // TODO Change the market value format!!!
+      this.parsedMarkets[convertMarketNameToHumingbotMarketName(market.name)] =
+        market;
+    }
+  }
+
+  async markets(req: ClobMarketsRequest): Promise<{ markets: MarketInfo }> {
+    if (req.market && req.market in this.parsedMarkets)
+      return { markets: this.parsedMarkets[req.market] };
+
+    return { markets: Object.values(this.parsedMarkets) };
+  }
+
+  async orderBook(req: ClobOrderbookRequest): Promise<Orderbook> {
+    const orderBook = await this.kujira.getOrderBook({
+      marketName: convertHumingbotMarketNameToMarketName(req.market),
+    });
+
+    const buys = [];
+    for (const order of orderBook.bids.valueSeq()) {
+      buys.push({
+        price: order.price ? order.price.toString() : '',
+        quantity: order.amount.toString(),
+        timestamp: order.creationTimestamp ? order.creationTimestamp : 0,
+      });
+    }
+
+    const sells = [];
+    for (const order of orderBook.asks.valueSeq()) {
+      sells.push({
+        price: order.price ? order.price.toString() : '',
+        quantity: order.amount.toString(),
+        timestamp: order.creationTimestamp ? order.creationTimestamp : 0,
+      });
+    }
+
+    return { buys, sells };
+  }
+
+  async orders(
+    req: ClobGetOrderRequest
+  ): Promise<{ orders: ClobGetOrderResponse['orders'] }> {
+    const order = await this.kujira.getOrder({
+      id: req.orderId,
+      marketName: convertHumingbotMarketNameToMarketName(req.market),
+      ownerAddress: getNotNullOrThrowError<OwnerAddress>(req.address),
+    });
+
+    return {
+      orders: [
+        // TODO Check this convertor!!!
+        {
+          network: this.network ? this.network : '',
+          timestamp: order.creationTimestamp
+            ? order.creationTimestamp.toString()
+            : '',
+          latency: '',
+          id: order.id ? order.id : '',
+          marketName: order.marketName,
+          marketId: order.marketId,
+          market: JSON.stringify(order.market),
+          ownerAddress: order.ownerAddress ? order.ownerAddress : '',
+          price: order.price ? order.price.toString() : '',
+          amount: order.amount ? order.amount.toString() : '',
+          side: order.side,
+          status: order.status ? order.status : '',
+          type: order.type ? order.type : '',
+          fee: order.fee ? order.fee.toString() : '',
+          creationTimestamp: order.creationTimestamp
+            ? order.creationTimestamp.toString()
+            : '',
+          fillingTimestamp: order.fillingTimestamp
+            ? order.fillingTimestamp.toString()
+            : '',
+          hashes: order.hashes ? JSON.stringify(order.hashes) : '',
+        },
+      ],
+    };
+  }
+
+  async postOrder(req: ClobPostOrderRequest): Promise<{ txHash: string }> {
+    const result = await this.kujira.placeOrder({
+      marketName: convertHumingbotMarketNameToMarketName(req.market),
+      ownerAddress: req.address,
+      side: req.side as OrderSide,
+      price: BigNumber(req.price),
+      amount: BigNumber(req.amount),
+      type: req.orderType as OrderType,
+    });
+
+    return { txHash: getNotNullOrThrowError<string>(result.hashes?.creation) };
+  }
+
+  public async ticker(
+    req: ClobTickerRequest
+  ): Promise<{ markets: MarketInfo }> {
+    // TODO check the output format!!!
+    return await this.markets(req);
   }
 }
--- src/connectors/kujira/kujira.ts
+++ src/connectors/kujira/kujira.ts
@@ -105,21 +105,6 @@ import {
   TransactionHash,
   Withdraw,
 } from './kujira.types';
-import {
-  ClobDeleteOrderRequest,
-  ClobGetOrderRequest,
-  ClobGetOrderResponse,
-  ClobMarketsRequest,
-  ClobOrderbookRequest,
-  ClobPostOrderRequest,
-  ClobTickerRequest,
-} from '../../clob/clob.requests';
-import {
-  CLOBish,
-  MarketInfo,
-  NetworkSelectionRequest,
-  Orderbook,
-} from '../../services/common-interfaces';
 import { KujiraConfig, NetworkConfig } from './kujira.config';
 import { Slip10RawIndex } from '@cosmjs/crypto';
 import {
@@ -143,7 +128,6 @@ import {
 import contracts from 'kujira.js/src/resources/contracts.json';
 import axios from 'axios';
 import {
-  convertHumingbotMarketNameToMarketName,
   convertKujiraBalancesToBalances,
   convertKujiraEventsToMapOfEvents,
   convertKujiraMarketToMarket,
@@ -154,7 +138,6 @@ import {
   convertKujiraTickerToTicker,
   convertKujiraTokenToToken,
   convertKujiraTransactionToTransaction,
-  convertMarketNameToHumingbotMarketName,
   convertNetworkToKujiraNetwork,
   convertNonStandardKujiraTokenIds,
 } from './kujira.convertors';
@@ -197,7 +180,7 @@ const config = KujiraConfig.config;
 /**
  *
  */
-export class Kujira implements CLOBish {
+export class Kujira {
   /**
    *
    * @private
@@ -277,8 +260,6 @@ export class Kujira implements CLOBish {
    *
    */
   isReady: boolean = false;
-
-  public parsedMarkets: MarketInfo = [];
 
   /**
    * Get the Kujira instance for the given chain and network.
@@ -2011,146 +1992,5 @@ export class Kujira implements CLOBish {
     latencies.sort((a, b) => a.latency - b.latency);
 
     return latencies[0].endpoint;
-  }
-
-  // CLOB Methods
-
-  public ready(): boolean {
-    return this.isReady;
-  }
-
-  async deleteOrder(req: ClobDeleteOrderRequest): Promise<{ txHash: string }> {
-    const result = await this.cancelOrder({
-      id: req.orderId,
-      marketName: convertHumingbotMarketNameToMarketName(req.market),
-      ownerAddress: req.address,
-    });
-
-    return {
-      txHash: getNotNullOrThrowError<TransactionHash>(
-        result.hashes?.cancellation
-      ),
-    };
-  }
-
-  estimateGas(_req: NetworkSelectionRequest): {
-    gasPrice: number;
-    gasPriceToken: string;
-    gasLimit: number;
-    gasCost: number;
-  } {
-    const result = this.getEstimatedFees({});
-
-    return {
-      gasCost: result.cost.toNumber(),
-      gasLimit: result.limit.toNumber(),
-      gasPrice: result.price.toNumber(),
-      gasPriceToken: result.token,
-    };
-  }
-
-  async loadMarkets(): Promise<void> {
-    const allMarkets = (await this.getAllMarkets()) as GetAllMarketsResponse;
-
-    for (const market of allMarkets.values()) {
-      // TODO Change the market value format!!!
-      this.parsedMarkets[convertMarketNameToHumingbotMarketName(market.name)] =
-        market;
-    }
-  }
-
-  async markets(req: ClobMarketsRequest): Promise<{ markets: MarketInfo }> {
-    if (req.market && req.market in this.parsedMarkets)
-      return { markets: this.parsedMarkets[req.market] };
-
-    return { markets: Object.values(this.parsedMarkets) };
-  }
-
-  async orderBook(req: ClobOrderbookRequest): Promise<Orderbook> {
-    const orderBook = await this.getOrderBook({
-      marketName: convertHumingbotMarketNameToMarketName(req.market),
-    });
-
-    const buys = [];
-    for (const order of orderBook.bids.valueSeq()) {
-      buys.push({
-        price: order.price ? order.price.toString() : '',
-        quantity: order.amount.toString(),
-        timestamp: order.creationTimestamp ? order.creationTimestamp : 0,
-      });
-    }
-
-    const sells = [];
-    for (const order of orderBook.asks.valueSeq()) {
-      sells.push({
-        price: order.price ? order.price.toString() : '',
-        quantity: order.amount.toString(),
-        timestamp: order.creationTimestamp ? order.creationTimestamp : 0,
-      });
-    }
-
-    return { buys, sells };
-  }
-
-  async orders(
-    req: ClobGetOrderRequest
-  ): Promise<{ orders: ClobGetOrderResponse['orders'] }> {
-    const order = await this.getOrder({
-      id: req.orderId,
-      marketName: convertHumingbotMarketNameToMarketName(req.market),
-      ownerAddress: getNotNullOrThrowError<OwnerAddress>(req.address),
-    });
-
-    return {
-      orders: [
-        // TODO Check this convertor!!!
-        {
-          network: this.network ? this.network : '',
-          timestamp: order.creationTimestamp
-            ? order.creationTimestamp.toString()
-            : '',
-          latency: '',
-          id: order.id ? order.id : '',
-          marketName: order.marketName,
-          marketId: order.marketId,
-          market: JSON.stringify(order.market),
-          ownerAddress: order.ownerAddress ? order.ownerAddress : '',
-          price: order.price ? order.price.toString() : '',
-          amount: order.amount ? order.amount.toString() : '',
-          side: order.side,
-          status: order.status ? order.status : '',
-          type: order.type ? order.type : '',
-          fee: order.fee ? order.fee.toString() : '',
-          creationTimestamp: order.creationTimestamp
-            ? order.creationTimestamp.toString()
-            : '',
-          fillingTimestamp: order.fillingTimestamp
-            ? order.fillingTimestamp.toString()
-            : '',
-          hashes: order.hashes ? JSON.stringify(order.hashes) : '',
-        },
-      ],
-    };
-  }
-
-  async postOrder(req: ClobPostOrderRequest): Promise<{ txHash: string }> {
-    const request = {
-      marketName: convertHumingbotMarketNameToMarketName(req.market),
-      ownerAddress: req.address,
-      side: req.side as OrderSide,
-      price: BigNumber(req.price),
-      amount: BigNumber(req.amount),
-      type: req.orderType as OrderType,
-    };
-    const result = await this.placeOrder(request);
-
-    return { txHash: getNotNullOrThrowError<string>(result.hashes?.creation) };
-  }
-
-  public async ticker(
-    req: ClobTickerRequest
-  ): Promise<{ markets: MarketInfo }> {
-    // TODO check the output format!!!
-    return await this.markets(req);
   }
 }
