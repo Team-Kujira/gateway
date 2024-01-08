8f39d6062cd8d8c15e7b212234570678bc0dedf2
Álvaro E. F. Mota <alvaro.mota@funttastic.com>
2024-01-08 16:09:30 -0300

    Added CLOBish interface and functions

--- src/connectors/kujira/kujira.ts
+++ src/connectors/kujira/kujira.ts
@@ -1,4 +1,9 @@
 import {
+  CLOBish,
+  MarketInfo,
+} from '../../services/common-interfaces';
+
+import {
   Address,
   AllMarketsWithdrawsRequest,
   AllMarketsWithdrawsResponse,
@@ -141,6 +146,7 @@ import {
   convertKujiraTransactionToTransaction,
   convertNetworkToKujiraNetwork,
   convertNonStandardKujiraTokenIds,
+  convertMarketNameToHumingbotMarketName,
 } from './kujira.convertors';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Cache, CacheContainer } from 'node-ts-cache';
@@ -183,7 +189,10 @@ const config = KujiraConfig.config;
 /**
  *
  */
-export class Kujira {
+export class Kujira implements CLOBish {
+
+  public parsedMarkets: MarketInfo = {};
+
   /**
    *
    * @private
@@ -2152,4 +2161,221 @@ export class Kujira {
 
     return latencies[0].endpoint;
   }
+
+
+  // CLOB section
+  // !!!TODO replace all this.kujira. to this.
+
+  async loadMarkets(): Promise<void> {
+    const allMarkets =
+      (await this.kujira.getAllMarkets()) as GetAllMarketsResponse;
+
+    for (const market of allMarkets.values()) {
+      this.parsedMarkets[convertMarketNameToHumingbotMarketName(market.name)] =
+        market;
+    }
+  }
+
+  ready(): boolean { //!!!TODO verify this.kujira statement
+    return this.kujira && this.kujira.isReady;
+  }
+
+  async markets(req: ClobMarketsRequest): Promise<{ markets: MarketInfo }> {
+    if (req.market && req.market.split('-').length === 2) {
+      const resp: CLOBMarkets = {};
+      resp[req.market] = this.parsedMarkets[req.market];
+
+      return { markets: resp };
+    }
+
+    return { markets: this.parsedMarkets };
+  }
+
+  async orderBook(req: ClobOrderbookRequest): Promise<Orderbook> {
+    const orderBook = await this.kujira.getOrderBook({
+      marketName: convertHumingbotMarketNameToMarketName(req.market),
+    });
+
+    const buys = [];
+    for (const order of orderBook.bids.valueSeq()) {
+      buys.push({
+        price: getNotNullOrThrowError<OrderPrice>(order.price).toString(),
+        quantity: getNotNullOrThrowError<OrderAmount>(order.amount).toString(),
+        timestamp: order.creationTimestamp ? order.creationTimestamp : 0,
+      });
+    }
+
+    const sells = [];
+    for (const order of orderBook.asks.valueSeq()) {
+      sells.push({
+        price: getNotNullOrThrowError<OrderPrice>(order.price).toString(),
+        quantity: getNotNullOrThrowError<OrderAmount>(order.amount).toString(),
+        timestamp: order.creationTimestamp ? order.creationTimestamp : 0,
+      });
+    }
+
+    return { buys, sells };
+  }
+
+  public async ticker(
+    req: ClobTickerRequest
+  ): Promise<{ markets: MarketInfo }> {
+    const requestMarket = getNotNullOrThrowError<string>(req.market);
+    const ticker = await this.kujira.getTicker({
+      marketName: convertHumingbotMarketNameToMarketName(requestMarket),
+    });
+    const marketMap: { [key: string]: any } = {};
+    marketMap[requestMarket] = {
+      market: ticker.market,
+      ticker: ticker.ticker,
+      price: ticker.price,
+      timestamp: ticker.timestamp,
+    };
+
+    return { markets: marketMap };
+  }
+
+  async orders(
+    req: ClobGetOrderRequest
+  ): Promise<{ orders: ClobGetOrderResponse['orders'] }> {
+    let originalOrders;
+
+    if (req.orderId) {
+      const originalOrder = await this.kujira.getOrder({
+        id: req.orderId,
+        marketName: convertHumingbotMarketNameToMarketName(req.market),
+        ownerAddress: getNotNullOrThrowError<OwnerAddress>(req.address),
+      });
+
+      originalOrders = [originalOrder];
+    } else {
+      originalOrders = getNotNullOrThrowError<IMap<OrderId, Order>>(
+        await this.kujira.getOrders({
+          marketName: convertHumingbotMarketNameToMarketName(req.market),
+          ownerAddress: getNotNullOrThrowError<OwnerAddress>(req.address),
+        })
+      )
+        .valueSeq()
+        .toArray();
+    }
+
+    const orders = [];
+
+    for (const originalOrder of originalOrders) {
+      if (originalOrder) {
+        const order = {
+          id: getNotNullOrThrowError<OrderId>(originalOrder.id),
+          clientId: originalOrder.clientId,
+          orderHash: '',
+          marketId: originalOrder.marketId,
+          active: '',
+          subaccountId: '',
+          executionType: '',
+          orderType: getNotNullOrThrowError<OrderType>(originalOrder.type),
+          price: getNotNullOrThrowError<OrderPrice>(
+            originalOrder.price
+          ).toString(),
+          triggerPrice: '',
+          quantity: originalOrder.amount.toString(),
+          filledQuantity: '',
+          state: getNotNullOrThrowError<OrderStatus>(originalOrder.status),
+          createdAt: originalOrder.creationTimestamp
+            ? originalOrder.creationTimestamp.toString()
+            : '',
+          updatedAt: originalOrder.fillingTimestamp
+            ? originalOrder.fillingTimestamp.toString()
+            : '',
+          direction: originalOrder.side,
+        };
+
+        orders.push(order);
+      }
+    }
+
+    return { orders } as {
+      orders:
+        | [
+        {
+          [key: string]: string;
+        }
+      ]
+        | [];
+    };
+  }
+
+  async postOrder(
+    req: ClobPostOrderRequest
+  ): Promise<{ txHash: string; id?: string }> {
+    const result = await this.kujira.placeOrder({
+      clientId: req.clientOrderID,
+      marketName: convertHumingbotMarketNameToMarketName(req.market),
+      ownerAddress: req.address,
+      side: req.side as OrderSide,
+      price: BigNumber(req.price),
+      amount: BigNumber(req.amount),
+      type: req.orderType as OrderType,
+    });
+
+    return {
+      txHash: getNotNullOrThrowError<string>(result.hashes?.creation),
+      id: result.id,
+    };
+  }
+
+  async deleteOrder(req: ClobDeleteOrderRequest): Promise<{ txHash: string }> {
+    if (req.orderId) {
+      const result = await this.kujira.cancelOrder({
+        id: req.orderId,
+        marketName: convertHumingbotMarketNameToMarketName(req.market),
+        ownerAddress: req.address,
+      });
+
+      return {
+        txHash: getNotNullOrThrowError<TransactionHash>(
+          result.hashes?.cancellation
+        ),
+      };
+    } else {
+      const result = await this.kujira.cancelAllOrders({
+        marketName: convertHumingbotMarketNameToMarketName(req.market),
+        ownerAddress: req.address,
+      });
+
+      if (result.size) {
+        const order: Order = getNotNullOrThrowError(result.first());
+        const order_hash: OrderTransactionHashes =
+          getNotNullOrThrowError<OrderTransactionHashes>(order.hashes);
+        let hash: string | undefined;
+        if ('creation' in order_hash) {
+          hash = order_hash.creation;
+        } else if ('cancellation' in order_hash) {
+          hash = order_hash.cancellation;
+        } else if ('withdraw' in order_hash) {
+          hash = order_hash.withdraw;
+        }
+
+        return { txHash: getNotNullOrThrowError<string>(hash) };
+      } else {
+        return { txHash: '' };
+      }
+    }
+  }
+
+  estimateGas(_req: NetworkSelectionRequest): {
+    gasPrice: number;
+    gasPriceToken: string;
+    gasLimit: number;
+    gasCost: number;
+  } {
+    const result = this.kujira.getEstimatedFees({});
+
+    return {
+      gasCost: result.cost.toNumber(),
+      gasLimit: result.limit.toNumber(),
+      gasPrice: result.price.toNumber(),
+      gasPriceToken: result.token,
+    };
+  }
+
+
 }
